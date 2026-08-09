3462a4cb7b6ea5f74ad820b4cd2210f2b6918d1d
Anna Dąbrowska <kubajka.anna8@gmail.com>
2026-08-09 23:39:48 +0200

    add link status checks for home page - test case 2

---
+++ tests/links-statuses.spec.ts
@@ -0,0 +1,15 @@
+import {test, expect} from '@playwright/test'
+import { getLinks, normalizeLinks } from '../src/utils/links' 
+
+
+test('all links on the home page return successful status codes', async ({ page, request }) => {
+  await page.goto('./');
+  const fetchedHrefs = await getLinks(page);
+  const links = normalizeLinks(fetchedHrefs, page.url())
+  expect(links.length).toBeGreaterThan(0)
+  
+  for (const url of links) {
+    const response = await request.get(url);
+    expect.soft(response.status(), `${url} responded with ${response.status()}`).toBeLessThan(400);
+  }
+});
